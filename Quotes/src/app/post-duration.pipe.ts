fbd9ddaff1c89726b189ae86d6d4bf6e398c1859
edwinaringo <tedaringo628@gmail.com>
2021-03-29 00:51:22 +0300

    Adding custom pipe functionality

--- Quotes/src/app/post-duration.pipe.ts
+++ Quotes/src/app/post-duration.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PostDurationPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number{
+    let today:Date = new Date(); // this gets the current date and time
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference =Math.abs(todayWithNoTime - value)// this will return value in milliseconds
+    const secondsInADay= 86400; //60 seconds multiplied by 60 minutes in an hour by 24 hours
+
+    var dateDifferenceSeconds=dateDifference*0.001; // this will convert it to seconds
+
+    var dateCounter = dateDifferenceSeconds/secondsInADay;
+
+    if (dateCounter >= 1){
+        return dateCounter;
+    }else{
+        return 0;
+
+  }
   }
 
 }
