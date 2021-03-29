c26986add5792a0c4f5d2f3b92542295d72b3bd4
edwinaringo <tedaringo628@gmail.com>
2021-03-29 14:26:59 +0300

    Correcting spelling erroes

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
   userQuotes = [
     new Quote(1, 'Love for all, hatred for none','-Margot Wale',0,0, new Date(2021,3,23)),
     new Quote(2, 'For all the kids that dream the impossible','-Lewis Hamilton',0,0, new Date(2021,3,23)),
-    new Quote(3, 'First you have to finsh','-Micheal Schumacher',0,0, new Date(2021,3,23)),
+    new Quote(3, 'First you have to finish','-Michael Schumacher',0,0, new Date(2021,3,23)),
     new Quote(4, 'Race cars become beautiful when they win','-Enzo Ferrari',0,0, new Date(2021,3,23)),
   ]
 
